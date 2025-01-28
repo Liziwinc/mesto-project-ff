1eb504afc1b80307e01fa24a4f2b6a5def3ec05d
Misha Kokorev <mishkokorev35@gmail.com>
2025-01-28 15:53:33 +0300

    0.1a

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,23 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
+loadCards(initialCards);
+
+function deleteCard(element){
+  element.remove();
+}
+
+function loadCards(array){
+  array.forEach(function (element) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = element.link;
+    cardElement.querySelector('.card__title').textContent = element.name;
+    
+    cardElement.querySelector('.card__delete-button').addEventListener('click', () => deleteCard(cardElement));
+
+    placesList.append(cardElement); 
+})}
